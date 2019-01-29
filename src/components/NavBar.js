6d12d8f06639b81a306c9e051e676f875f4b1faa
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-29 18:19:31 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ const NavBar = () => {
     <div>
       <NavLink to='/' exact >Home</NavLink>
       <NavLink to='/movies' exact >Movies</NavLink>
-      <NavLink to='/actors' exact >Actors</NavLink>
       <NavLink to='/directors' exact >Directors</NavLink>
+      <NavLink to='/actors' exact >Actors</NavLink>
     </div>
   );
 };
